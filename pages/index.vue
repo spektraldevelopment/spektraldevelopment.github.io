f60032cfe7d7a01a61ac05a3f0a9d37b3c7c1316
Dave Boyle <foo@bar.com>
2019-11-29 19:26:24 -0500

    [develop] - Added new banner background.

--- pages/index.vue
+++ pages/index.vue
@@ -1,13 +1,13 @@
 <template>
   <v-content>
     <v-container pa-0 fluid>
-      <v-row justify="center" align="center" no-gutters class="purple darken-2 pa-4">
-        <v-col cols="12" md="4" class="text-center">
+      <v-row justify="center" align="center" no-gutters class="purple darken-2 pa-4 back">
+        <v-col cols="12" md="4" lg="3" class="text-center">
           <v-avatar size="250">
             <img src="https://cdn.vuetifyjs.com/images/john.jpg" alt="John" />
           </v-avatar>
         </v-col>
-        <v-col cols="12" md="4" justify-self="center" class="text-center">
+        <v-col cols="12" md="4" lg="3" justify-self="center" class="text-center">
           <h1>Hello, I'm Dave</h1>
           <p>I'm a Senior Software Engineer in Toronto</p>
         </v-col>
@@ -24,10 +24,9 @@ h1,
 p {
   color: #fff;
 }
-.absolute-center {
-  position: absolute;
-  top: 50%;
-  left: 50%;
-  transform: translate(-50%, -50%);
+
+.back {
+  background-image: url(/images/banner-dark.jpg);
+  background-size: cover;
 }
 </style>
